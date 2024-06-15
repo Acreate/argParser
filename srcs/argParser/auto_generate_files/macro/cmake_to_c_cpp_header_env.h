85d62653a9d41c1952b04e2c3d538bfae8d18412
Acreate <1552397900@qq.com>
2024-06-15 13:30:30 +0800

    提交模板

--- srcs/argParser/auto_generate_files/macro/cmake_to_c_cpp_header_env.h
+++ srcs/argParser/auto_generate_files/macro/cmake_to_c_cpp_header_env.h
@@ -10,12 +10,12 @@
 #define is_exe false
 
 #define Cmake_Project_Name u8"argParser"
-#define Project_Run_bin u8"D:/userCodes/qt/QReptileIntroduce/builder/Release_x64_MSVC/"
-#define Project_Plug_bin u8"D:/userCodes/qt/QReptileIntroduce/builder/Release_x64_MSVC/plugs"
+#define Project_Run_bin u8"D:/userCodes/qt/QReptileIntroduce/builder/Debug_x64_MSVC/"
+#define Project_Plug_bin u8"D:/userCodes/qt/QReptileIntroduce/builder/Debug_x64_MSVC/plugs"
 #define Cmake_Executable_Suffix u8".exe"
 #define Cmake_Shared_Library_Suffix u8".dll"
 #define project_type u8"SHARED_LIBRARY"
 
-#define Cache_Path_Dir u8"D:/userCodes/qt/QReptileIntroduce/builder/Release_x64_MSVC/write_cache/"
+#define Cache_Path_Dir u8"D:/userCodes/qt/QReptileIntroduce/builder/Debug_x64_MSVC/write_cache/"
 
 #endif // CMAKE_TO_C_CPP_HEADER_ENV_H_H_HEAD__FILE__
